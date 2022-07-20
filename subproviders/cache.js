deb382902de32d2758a7a87e5829a7f57e4e96f9
sam bacha <sam@manifoldfinance.com>
2022-07-20 15:45:14 -0700

    Update cache.js

--- subproviders/cache.js
+++ subproviders/cache.js
@@ -1,10 +1,10 @@
-const ProviderSubprovider = require('./json-rpc-engine-middleware')
-const createBlockCacheMiddleware = require('eth-json-rpc-middleware/block-cache')
+const ProviderSubprovider = require('./json-rpc-engine-middleware');
+const { createBlockCacheMiddleware } = require('eth-json-rpc-middleware/dist/block-cache');
 
 class BlockCacheSubprovider extends ProviderSubprovider {
   constructor(opts) {
     super(({ blockTracker }) => createBlockCacheMiddleware(Object.assign({ blockTracker }, opts)))
   }
-}
+};
 
 module.exports = BlockCacheSubprovider
